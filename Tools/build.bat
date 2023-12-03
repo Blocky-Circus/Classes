455e60eb16beea0e9f22a9a0cfb0e094dbfb3058
DartRuffian <skulldude327@gmail.com>
2023-12-02 21:00:03 -0600

    Updated build script to take option

--- Tools/build.bat
+++ Tools/build.bat
@@ -1,4 +1,12 @@
-set source="C:\Coding\GitHub Repos\BC-Classes\source"
+@echo off
+if "%~1"=="" (
+    set option=\cobblemon
+) else (
+    set option=%1
+)
+
+set source="C:\Coding\GitHub Repos\BC-Classes\source\%option%"
+echo %source%
 set target="%appdata%\com.modrinth.theseus\profiles\BC Cobblemon [TEST]\saves\Origin Testing\datapacks"
 
 rmDir /s /q %target%
